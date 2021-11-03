c40dcdc16a24f279c91b108f36d3d8c7ef44cd0c
yuanzhitang <Yuanzhi.Tang@cn.unisys.com>
2021-11-04 00:13:32 +0800

    Add Script to build Identity Image

--- src/Services/Identity/Identity.API/Startup.cs
+++ src/Services/Identity/Identity.API/Startup.cs
@@ -33,7 +33,6 @@ namespace Identity.API
 			Configuration.Bind("JwtSettings", jwtSettings);
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
