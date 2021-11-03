674e1cb44b51bd26095902a4dc272b9fd1bf3b52
yuanzhitang <Yuanzhi.Tang@cn.unisys.com>
2021-11-03 22:13:09 +0800

    Refine Authorize logic

--- src/Services/Identity/Identity.API/Startup.cs
+++ src/Services/Identity/Identity.API/Startup.cs
@@ -22,11 +22,6 @@ namespace Identity.API
 
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddAuthentication("OAuth")
-				.AddJwtBearer("OAuth", config =>
-				{
-
-				});
 			services.AddControllers();
 			services.AddSwaggerGen(c =>
 			{
@@ -37,35 +32,33 @@ namespace Identity.API
 			var jwtSettings = new JwtSettings();
 			Configuration.Bind("JwtSettings", jwtSettings);
 
-			//services.AddAuthentication(option =>
-			//{
-			//	option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-			//	option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-			//})
-			//.AddJwtBearer(options =>
-			//{
-			//	options.TokenValidationParameters = new TokenValidationParameters
-			//	{
-			//		ValidIssuer = jwtSettings.Issuer,
-			//		ValidAudience = jwtSettings.Audience,
-			//		IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey))
+			services.AddAuthentication("OAuth")
+			.AddJwtBearer("OAuth", options =>
+			{
+				var secretBytes = Encoding.UTF8.GetBytes(jwtSettings.SecretKey);
+				var key = new SymmetricSecurityKey(secretBytes);
+				options.TokenValidationParameters = new TokenValidationParameters
+				{
+					ValidIssuer = jwtSettings.Issuer,
+					ValidAudience = jwtSettings.Audience,
+					IssuerSigningKey = key
 
-			//		/***********************************TokenValidationParameters的参数默认值***********************************/
-			//		// RequireSignedTokens = true,
-			//		// SaveSigninToken = false,
-			//		// ValidateActor = false,
-			//		// 将下面两个参数设置为false，可以不验证Issuer和Audience，但是不建议这样做。
-			//		// ValidateAudience = true,
-			//		// ValidateIssuer = true, 
-			//		// ValidateIssuerSigningKey = false,
-			//		// 是否要求Token的Claims中必须包含Expires
-			//		// RequireExpirationTime = true,
-			//		// 允许的服务器时间偏移量
-			//		// ClockSkew = TimeSpan.FromSeconds(300),
-			//		// 是否验证Token有效期，使用当前时间与Token的Claims中的NotBefore和Expires对比
-			//		// ValidateLifetime = true
-			//	};
-			//});
+					/***********************************TokenValidationParameters的参数默认值***********************************/
+					//		// RequireSignedTokens = true,
+					//		// SaveSigninToken = false,
+					//		// ValidateActor = false,
+					//		// 将下面两个参数设置为false，可以不验证Issuer和Audience，但是不建议这样做。
+					//		// ValidateAudience = true,
+					//		// ValidateIssuer = true, 
+					//		// ValidateIssuerSigningKey = false,
+					//		// 是否要求Token的Claims中必须包含Expires
+					//		// RequireExpirationTime = true,
+					//		// 允许的服务器时间偏移量
+					//		// ClockSkew = TimeSpan.FromSeconds(300),
+					//		// 是否验证Token有效期，使用当前时间与Token的Claims中的NotBefore和Expires对比
+					//		// ValidateLifetime = true
+				};
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
