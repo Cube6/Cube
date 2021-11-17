d103c155345dfbebc9df4d8663b08d1d887670d3
wushuai <Shuai.Wu@unisys.com>
2021-11-17 17:16:26 +0800

    board change

--- src/Web/cube/src/components/board.vue
+++ src/Web/cube/src/components/board.vue
@@ -1,27 +1,60 @@
-<template>
-    <div class="board">
-        <div class="content">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Temp. (C)</th>
-                        <th>Temp. (F)</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="user in post" :key="user.id">
-                        <td>{{ user.id }}</td>
-                        <td>{{ user.name }}</td>
-                        <td>{{ user.avatarUrl }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+﻿<template>
+    <div class="layout">
+        <Layout :style="{minHeight: '100vh'}">
+            <Sider collapsible :collapsed-width="78" v-model="isCollapsed">
+                <Menu active-name="1-1" theme="dark" width="auto" :class="menuitemClasses">
+                    <MenuItem name="1-1">
+                        <Icon type="ios-navigate"></Icon>
+                        <span>Discussboard</span>
+                    </MenuItem>
+                    <MenuItem name="1-2">
+                        <Icon type="search"></Icon>
+                        <span>User</span>
+                    </MenuItem>
+                    <MenuItem name="1-3">
+                        <Icon type="settings"></Icon>
+                        <span>Settings</span>
+                    </MenuItem>
+                </Menu>
+            </Sider>
+            <Layout>
+                <Header :style="{background: '#fff', boxShadow: '0 2px 3px 2px rgba(0,0,0,.1)'}"></Header>
+                <Content :style="{padding: '0 16px 16px'}">
+                    <Breadcrumb :style="{margin: '16px 0'}">
+                        <BreadcrumbItem>Home</BreadcrumbItem>
+                        <BreadcrumbItem>Components</BreadcrumbItem>
+                        <BreadcrumbItem>Board</BreadcrumbItem>
+                    </Breadcrumb>
+                    <Card>
+                        <div style="height: 600px">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th width="200px">Id</th>
+                                        <th width="200px">Name</th>
+                                        <th width="200px">DateCreated</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="user in post" :key="user.id">
+                                        <td>{{ user.id }}</td>
+                                        <td>{{ user.name }}</td>
+                                        <td>{{ user.datecreated }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </Card>
+                </Content>
+            </Layout>
+        </Layout>
     </div>
 </template>
-
-<script lang="js">
+<script>
     import board from "../scripts/board.js";
     export default board;
 </script>
+
+<style>
+    @import "../styles/board.css";
+</style>
