cba58c570e92e429798efda5ffc2a5d258b0c89b
yuanzhitang <Yuanzhi.Tang@cn.unisys.com>
2021-11-28 07:53:40 +0800

    Add Empty Dto objects for Board and User

---
+++ src/Services/User/User.Application/Dtos/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube.User.Application.Dtos
+{
+	internal class UserDto
+	{
+	}
+}
