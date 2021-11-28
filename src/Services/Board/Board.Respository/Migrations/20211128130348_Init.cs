08137f5009aed8078ed413d615ffaeb673534d8a
yuanzhitang <Yuanzhi.Tang@cn.unisys.com>
2021-11-28 21:04:30 +0800

    Add Init Scripts for Board.Repo

---
+++ src/Services/Board/Board.Respository/Migrations/20211128130348_Init.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cube.Board.Respository.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DisscussionBoards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    CreatedUser = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DisscussionBoards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DisscussionBoardItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    Detail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BoardId = table.Column<int>(type: "int", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DisscussionBoardItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DisscussionBoardItems_DisscussionBoards_BoardId",
+                        column: x => x.BoardId,
+                        principalTable: "DisscussionBoards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DisscussionBoardItems_BoardId",
+                table: "DisscussionBoardItems",
+                column: "BoardId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DisscussionBoardItems");
+
+            migrationBuilder.DropTable(
+                name: "DisscussionBoards");
+        }
+    }
+}
