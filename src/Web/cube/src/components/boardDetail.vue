c6a4395a13e77821922407384884d6cdf489163f
wushuai <Shuai.Wu@unisys.com>
2021-12-01 16:26:59 +0800

    board change

--- src/Web/cube/src/components/boardDetail.vue
+++ src/Web/cube/src/components/boardDetail.vue
@@ -1,39 +1,75 @@
 ﻿<template>
-        <div style="height: 600px">
-            <table>
-                <thead>
-                    <tr>
-                        <th width="30%">What went well</th>
-                        <th width="40%">What need to be imporved</th>
-                        <th width="30%">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="board in post" :key="board.Id">
-                        <td>{{ board.Id }}</td>
-                        <td>{{ board.Name }}</td>
-                        <td>{{ board.DateCreated }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+    <div style="height: 600px">
+        <table>
+            <thead>
+                <tr>
+                    <th width="400px">What went well</th>
+                    <th width="400px">What need to be imporved</th>
+                    <th width="200px">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <ul>
+                            <li v-for="well in post" :key="well.Id">
+                                {{well.Name}}
+                            </li>
+                        </ul>
+                        <textarea></textarea>
+                        <Button type="primary" @click="AddWentWell()">Send</Button>
+                    </td>
+                    <td>
+                        <ul>
+                            <li v-for="imporve in post" :key="imporve.Id">
+                                {{imporve.Name}}
+                            </li>
+                        </ul>
+                        <textarea></textarea>
+                        <Button type="primary" @click="AddImporved()">Send</Button>
+                    </td>
+                    <td>
+                        <ul>
+                            <li v-for="action in post" :key="action.Id">
+                                {{action.Name}}
+                            </li>
+                        </ul>
+                        <textarea></textarea>
+                        <Button type="primary" @click="AddAction()">Send</Button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
-    //import axios from 'axios';
     export default {
         data() {
             return {
                 post: null
             };
         },
+        created() {
+            // fetch the data when the view is created and the data is
+            // already being observed
+            this.fetchData();
+        },
         methods: {
-            addDiscussionBoard(name) {
-                alert(name);
+            fetchData() {
+                fetch('Board')
+                    .then(r => r.json())
+                    .then(json => {
+                        this.post = json;
+                        return;
+                    });
             }
         },
     }
 </script>
 
 <style>
+    ul {
+        list-style-type: none;
+    }
 </style>
