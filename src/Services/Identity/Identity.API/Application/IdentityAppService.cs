d942e9db090ae4867340401e294fa44c9bd82d65
yuanzhitang <Yuanzhi.Tang@cn.unisys.com>
2021-12-14 13:00:43 +0800

    Move The address of UserService to config for Identity Service

--- src/Services/Identity/Identity.API/Application/IdentityAppService.cs
+++ src/Services/Identity/Identity.API/Application/IdentityAppService.cs
@@ -1,13 +1,22 @@
 ﻿using Grpc.Net.Client;
 using Cube.User.API.Protos;
+using Microsoft.Extensions.Configuration;
 
 namespace Cube.Identity.API.Application
 {
 	public class IdentityAppService : IIdentityAppService
 	{
+		public IdentityAppService(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
 		public bool Validate(string user, string password)
 		{
-			var channel = GrpcChannel.ForAddress("https://localhost:4000");
+			var address = Configuration["UserService:Address"];
+			var channel = GrpcChannel.ForAddress(address);
 			var client = new UserService.UserServiceClient(channel);
 
 			var result = client.Validate(new ValidateUserRequest()
