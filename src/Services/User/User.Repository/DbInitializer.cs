1553e312a66f02bf679d1f54dbcd4e143cdccd5d
yuanzhitang <Yuanzhi.Tang@cn.unisys.com>
2021-12-16 22:12:08 +0800

    Add Empty DbInitializer

---
+++ src/Services/User/User.Repository/DbInitializer.cs
@@ -0,0 +1,18 @@
+﻿using Cube.User.Respository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube.User.Repository
+{
+	internal class DbInitializer
+	{
+		public async static Task InitializeAsync(UserContext context)
+		{
+			await context.Database.MigrateAsync();
+		}
+	}
+}
