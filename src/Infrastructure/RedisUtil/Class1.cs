67c0f751497c9ce2bf45cacc600b4ea822b9db84
2257930660 <2257930660@qq.com>
2021-12-22 08:35:56 +0800

    Commit

--- src/Infrastructure/RedisUtil/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RedisUtil
-{
-	public class Class1
-	{
-	}
-}
---
+++ src/Infrastructure/RedisUtil/RedisRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using StackExchange.Redis;
+
+namespace RedisRepository
+{
+	public class RedisRepository : IDisposable
+	{
+		protected bool dispose;
+		public bool Disposed => dispose;
+
+		protected ConnectionMultiplexer multiplexer;
+
+		protected ReaderWriterLockSlim readerWriterLock;
+
+		public RedisRepository(string connectionString)
+		{
+			readerWriterLock = new ReaderWriterLockSlim();
+			multiplexer = ConnectionMultiplexer.Connect(connectionString);
+			dispose = false;
+		}
+
+		public void SetString(string key, string value)
+		{			
+		}
+
+		/// <summary>
+		/// Dispose
+		/// </summary>
+		/// <exception cref="ObjectDisposedException"></exception>
+		public void Dispose()
+		{
+			try
+			{
+				readerWriterLock.EnterUpgradeableReadLock();
+
+				if (Disposed)
+				{
+					throw new ObjectDisposedException(nameof(multiplexer));
+				}
+				else
+				{
+					readerWriterLock.EnterWriteLock();
+					multiplexer.Dispose();
+				}
+			}
+			finally
+			{
+				if (readerWriterLock.IsWriteLockHeld)
+					readerWriterLock.ExitWriteLock();
+
+				if (readerWriterLock.IsUpgradeableReadLockHeld)
+					readerWriterLock.ExitUpgradeableReadLock();
+			}
+		}
+	}
+}
