48b716ce50c05b32a6d136d7b28c7b292e32aadf
2257930660 <2257930660@qq.com>
2021-12-27 09:17:42 +0800

    Implementing

---
+++ src/Infrastructure/RedisUtil/RedisFactory.cs
@@ -0,0 +1,24 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisPractice
+{
+	public static class RedisFactory
+	{
+		public static async Task<IRedisInstance> GetInstanceAsync(string connection)
+		{
+			var redisRepository = new SimpleRedisRepository();
+			await redisRepository.InitializeAsync(connection);
+
+			var generator = new ProxyGenerator();
+			var proxy = generator.CreateInterfaceProxyWithTargetInterface<IRedisInstance>(redisRepository, redisRepository);
+
+			return proxy;
+		}
+	}
+}
--- src/Infrastructure/RedisUtil/RedisRepository.cs
+++ src/Infrastructure/RedisUtil/RedisRepository.cs
@@ -1,57 +1,138 @@
 ﻿using System;
 using System.Threading;
+using System.Reactive;
+using System.Threading.Tasks;
 using StackExchange.Redis;
+using Microsoft.VisualStudio.Threading;
 
-namespace RedisRepository
+namespace RedisPractice
 {
-	public class RedisRepository : IDisposable
+	internal abstract class RedisRepository : IDisposable
 	{
-		protected bool dispose;
-		public bool Disposed => dispose;
+		#region Events
+		public event EventHandler<InternalErrorEventArgs> InternalErrorHandler;
+		public event EventHandler<RedisErrorEventArgs> ErrorEventHandler;
+		public event EventHandler<ConnectionFailedEventArgs> ConnectionFailedHandler;
+		public event EventHandler<ConnectionFailedEventArgs> ConnectionRestoredHandler;
 
+		public void OnInternalError(object sender, InternalErrorEventArgs args)
+		{
+			InternalErrorHandler?.Invoke(sender, args);
+		}
+		public void OnErrorEvent(object sender, RedisErrorEventArgs args)
+		{
+			ErrorEventHandler?.Invoke(sender, args);
+		}
+		public void OnConnectionFailed(object sender, ConnectionFailedEventArgs args)
+		{
+			ConnectionFailedHandler?.Invoke(sender, args);
+		}
+		public void OnConnectionRestore(object sender, ConnectionFailedEventArgs args)
+		{
+			ConnectionRestoredHandler?.Invoke(sender, args);
+		}
+
+		#endregion
+
+		#region Properties and Fields
+		public bool Disposed => disposed;
+		private bool disposed = true;
+
+		//TODO : ReaderWriterLock can not span await statement.
+		protected AsyncReaderWriterLock disposeLock;
 		protected ConnectionMultiplexer multiplexer;
+		protected IDatabase database => multiplexer.GetDatabase();
 
-		protected ReaderWriterLockSlim readerWriterLock;
+		#endregion
 
-		public RedisRepository(string connectionString)
+		public RedisRepository()
 		{
-			readerWriterLock = new ReaderWriterLockSlim();
-			multiplexer = ConnectionMultiplexer.Connect(connectionString);
-			dispose = false;
+			disposeLock = new AsyncReaderWriterLock();
 		}
 
-		public void SetString(string key, string value)
-		{			
-		}
-
-		/// <summary>
-		/// Dispose
-		/// </summary>
-		/// <exception cref="ObjectDisposedException"></exception>
-		public void Dispose()
+		#region Public Methods
+		public async Task InitializeAsync(string connectionString)
 		{
+			AsyncReaderWriterLock.Releaser upLockReleaser = default, writeLockReleaser = default;
 			try
 			{
-				readerWriterLock.EnterUpgradeableReadLock();
+				upLockReleaser = await disposeLock.UpgradeableReadLockAsync();
 
 				if (Disposed)
 				{
-					throw new ObjectDisposedException(nameof(multiplexer));
-				}
-				else
-				{
-					readerWriterLock.EnterWriteLock();
-					multiplexer.Dispose();
+					writeLockReleaser = await disposeLock.WriteLockAsync();
+
+					multiplexer = await ConnectionMultiplexer.ConnectAsync(connectionString);
+
+					multiplexer.InternalError += OnInternalError;
+					multiplexer.ErrorMessage += OnErrorEvent;
+					multiplexer.ConnectionFailed += OnConnectionFailed;
+					multiplexer.ConnectionRestored += OnConnectionRestore;
+
+					disposed = false;
 				}
 			}
 			finally
 			{
-				if (readerWriterLock.IsWriteLockHeld)
-					readerWriterLock.ExitWriteLock();
+				if (disposeLock.IsWriteLockHeld)
+					await writeLockReleaser.ReleaseAsync();
 
-				if (readerWriterLock.IsUpgradeableReadLockHeld)
-					readerWriterLock.ExitUpgradeableReadLock();
+				if (disposeLock.IsUpgradeableReadLockHeld)
+					await upLockReleaser.ReleaseAsync();
 			}
 		}
+
+		public void Dispose()
+		{
+			DisposeAsync().Wait();
+		}
+		public async Task DisposeAsync()
+		{
+			AsyncReaderWriterLock.Releaser upLockReleaser = default, writeLockReleaser = default;
+			try
+			{
+				upLockReleaser = await disposeLock.UpgradeableReadLockAsync();
+				if (!disposed)
+				{
+					writeLockReleaser = await disposeLock.WriteLockAsync();
+
+					multiplexer.InternalError -= OnInternalError;
+					multiplexer.ErrorMessage -= OnErrorEvent;
+					multiplexer.ConnectionFailed -= OnConnectionFailed;
+					multiplexer.ConnectionRestored -= OnConnectionRestore;
+
+					multiplexer.Dispose();
+					disposed = true;
+				}
+			}
+			finally
+			{
+				if (disposeLock.IsWriteLockHeld)
+					await writeLockReleaser.ReleaseAsync();
+				if (disposeLock.IsUpgradeableReadLockHeld)
+					await upLockReleaser.ReleaseAsync();
+			}
+		}
+
+		#endregion
+
+		#region Private Methods
+		private async Task CheckDisposeAsync()
+		{
+			AsyncReaderWriterLock.Releaser readLockReleaser = default;
+			try
+			{
+				readLockReleaser = await disposeLock.ReadLockAsync();
+				if (disposed)
+					throw new ObjectDisposedException(nameof(multiplexer));
+			}
+			finally
+			{
+				if (disposeLock.IsReadLockHeld)
+					await readLockReleaser.ReleaseAsync();
+			}
+		}
+
+		#endregion
 	}
 }
