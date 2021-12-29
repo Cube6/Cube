e749b69b926a717bfcb88cfad22905049258fe50
yuanzhitang <Yuanzhi.Tang@cn.unisys.com>
2021-12-29 22:42:25 +0800

    Use HTTPS for all services

--- src/Services/Identity/Identity.API/Application/IdentityAppService.cs
+++ src/Services/Identity/Identity.API/Application/IdentityAppService.cs
@@ -2,6 +2,7 @@
 using Cube.User.API.Protos;
 using Microsoft.Extensions.Configuration;
 using System;
+using System.Net.Http;
 
 namespace Cube.Identity.API.Application
 {
@@ -20,7 +21,13 @@ namespace Cube.Identity.API.Application
 
 			Console.WriteLine($"Try to connect to the user service: {address}");
 
-			var channel = GrpcChannel.ForAddress(address);
+			var httpHandler = new HttpClientHandler();
+			// Return `true` to allow certificates that are untrusted/invalid
+			httpHandler.ServerCertificateCustomValidationCallback =
+				HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+			var channel = GrpcChannel.ForAddress(address,
+				new GrpcChannelOptions { HttpHandler = httpHandler });
 			var client = new UserService.UserServiceClient(channel);
 
 			var result = client.Validate(new ValidateUserRequest()
