10d03d8cc3d448e75c540e7c01fc19f63f7dfae8
yuanzhitang <Yuanzhi.Tang@cn.unisys.com>
2022-04-17 12:05:52 +0800

    Adjust Board Card Layout and Refine its tooltip

--- src/Web/cube/src/components/boardAll.vue
+++ src/Web/cube/src/components/boardAll.vue
@@ -16,10 +16,14 @@
             <li v-for="board in post" :key="board.Id" style="width:260px; float: left;">
                 <Card :style="{width: '250px', cursor: 'pointer', margin:'0px 0px 15px 0px', background: getBoardCardBG(board)}" v-on:click.native="ViewBoard(board)">
                     <p slot="title" :title="getBoardCardTooltip(board)">
-                        <img :src="getUserAvatar(board.CreatedUser)" :title="board.CreatedUser" style="width:20px; height:20px; border-radius:50%; " />
-                        <img src="../assets/Icons/completed.jpg" v-if="board.State == 2" title="Completed" style="width:20px; height:20px; border-radius:50%; float:right" >
-                        <img src="../assets/Icons/inprogress.jpg" v-if="board.IsDeleted==false && board.State == 1" title="In Progress" style="width:20px; height:20px; border-radius:50%; float:right" >
-                        <img src="../assets/Icons/deleted.png" v-if="board.IsDeleted" title="Deleted" style="width:20px; height:20px; border-radius:50%; float:right" >
+                        <span style="float:right">
+                            <img :src="getUserAvatar(board.CreatedUser)" :title="'Owner: ' + board.CreatedUser" style="width:20px; height:20px; border-radius:50%; " />
+                        </span>
+                       <span>
+                            <img v-if="board.State == 2" src="../assets/Icons/completed.jpg" title="Completed" style="width:20px; height:20px; border-radius:50%;" >           
+                            <img v-if="board.IsDeleted==false && board.State == 1" src="../assets/Icons/inprogress.jpg" title="In Progress" style="width:20px; height:20px; border-radius:50%; " >
+                            <img v-if="board.IsDeleted" src="../assets/Icons/deleted.png" title="Deleted" style="width:20px; height:20px; border-radius:50%;" >
+                        </span>
                     </p>
                     <div style="text-align: center; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
                         <a href="#" slot="extra" :title="getBoardCardTooltip(board)">
