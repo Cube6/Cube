18898a6cfbed5bb4ccdc3a1b33518338cd886169
2257930660 <2257930660@qq.com>
2022-04-21 07:56:04 +0800

    Adjust project reference

---
+++ src/Infrastructure/MessageQueue/IMessageQueue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMq
+{
+	public interface IMessageQueue
+	{
+		void Publish(string queueName, string message);
+		Guid Subscribe(string queueName, Action<string> messageHandler);
+		string Get(string queueName);
+		void UnSubscribe(Guid consumerGuid);
+	}
+}
